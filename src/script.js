c0654858ab65103a762943914468156874793022
Bernardo <bernardoalvesbr2025@gmail.com>
2025-07-10 14:57:54 -0300

    feat(ui): finish complete layoute and styling for gallery page

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll("div.flex > button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+  });
+});
